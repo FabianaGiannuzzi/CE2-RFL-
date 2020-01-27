159bfea102292cc3840c57cee9dd243a218a2559
Anna Ludovica Vinelli <annaludovica.vinelli@studenti.unimi.it>
2020-01-27 16:49:38 +0100

    What are web crawlers

--- src/Point_1.R
+++ src/Point_1.R
@@ -181,6 +181,7 @@ articoli_archivio_2016
 #Check out the RCrawler package and its accompanying paper. What does it me an to “ crawl” ? and what is it a “web spider” ? How is this different from a scraper you have built at point 5? 
 #Inspect the package documentation and sketch how you could bui ld a spider scraper: which function(s) should you use? With which arguments? Don't do it, just sketch and explain.
 
+## According to Salim Khalil and Mohamed Fakir, Web crawlers are programs used to retrieve and collect data from the web. They browse and download web pages in an automated way.
 
 
 
